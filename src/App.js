27dcecc58f86304e3eca245116aefdc3fef5059d
huy142000 <lehaminhhuy.93@gmail.com>
2022-11-02 12:19:25 +0700

    daythu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { createBrowserHistory } from "history";
 import { BrowserRouter, Route, Router, Routes, Switch } from "react-router-dom";
 import "./App.css";
+import BannerComponent from "./components/BannerComponent/BannerComponent";
 import Header from "./components/HeaderComponent/HeaderRFC";
 import Home from "./pages/Home/Home";
+
 export const history = createBrowserHistory();
 
 function App() {
@@ -13,8 +15,9 @@ function App() {
         <Switch>
           <Route exact path="/home" component={Home}></Route>
 
-          {/* default url */}
-          <Route exact path="/" component={Home}></Route>
+          {/* default url khi push nhánh nhớ đưa file vào Home*/}
+          {/* <Route exact path="/" component={Home}></Route> */}
+          <Route exact path="/" component={BannerComponent}></Route>
         </Switch>
       </Router>
     </BrowserRouter>
--- src/components/BannerComponent/BannerComponent.jsx
+++ src/components/BannerComponent/BannerComponent.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import Slider from "react-slick";
+import styles from "./banner.module.css";
 export default function BannerComponent() {
+  const settings = {
+    dots: true,
+    fade: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  console.log(styles);
   return (
-    <div>BannerComponent</div>
-  )
+    <div className="bg-dark ">
+      <h2>Fade</h2>
+      <Slider {...settings}>
+        <div className={styles.banner}></div>
+      </Slider>
+    </div>
+  );
 }
