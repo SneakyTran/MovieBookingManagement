fc73123dfb550ea75ab8797ae7b259316311b1f8
SneakyTurtle <le741996cf@gmail.com>
2022-11-02 07:39:41 +0700

    old

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from "react";
+import "./home.css";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+    return <div>abcxz</div>;
 }
