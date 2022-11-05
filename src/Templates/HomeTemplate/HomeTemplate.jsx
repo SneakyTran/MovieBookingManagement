a9251a8be26adb022362c7dd1bab029b4b5c1fe5
hson <phanhongson22097@gmail.com>
2022-11-05 16:44:51 +0700

    update footer

--- src/Templates/HomeTemplate/HomeTemplate.jsx
+++ src/Templates/HomeTemplate/HomeTemplate.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { Route } from "react-router-dom";
+import Footer from "../../components/FooterComponent/FooterRFC";
 import Header from "../../components/HeaderComponent/HeaderRFC";
 
 export default function HomeTemplate(props) {
@@ -12,6 +13,7 @@ export default function HomeTemplate(props) {
           <Fragment>
             <Header {...propRoute} />
             <Component {...propRoute} />
+            <Footer {...propRoute} />
           </Fragment>
         );
       }}
