9d0020369ea2412584c7ae2c6a90dd0d6aa974d2
hson <phanhongson220297@gmail.com>
2022-11-09 09:40:50 +0700

    update code login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,32 +1,47 @@
 import React from 'react';
 import styles from "./login.module.css";
-
+import { useDispatch, useSelector } from 'react-redux';
+import { loginAction } from '../../redux/actions/FormAction';
+import { useFormik } from 'formik';
+import { CLOSE_MODAL } from '../../redux/type/ModalType';
 
 
 export default function Login() {
+
+  let dispatch = useDispatch();
+  useSelector(state => state.ModalFilmReducer);
+  const formik = useFormik({
+    initialValues: {
+      taiKhoan: '',
+      matKhau: ''
+    },
+    onSubmit: values => {
+      console.log(values);
+      let action = loginAction(values);
+      dispatch(action);
+      dispatch({ type: CLOSE_MODAL })
+    },
+  })
+
   return (
     <div className={styles.login_box}>
       <h2>Login</h2>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <div className={styles.user_box}>
-          <input type="text" name="username" required />
-          <label>Username</label>
+          <input onChange={formik.handleChange} type="text" name="taiKhoan" required />
+          <label>Account</label>
         </div>
         <div className={styles.user_box}>
-          <input type="password" name="password" required />
+          <input onChange={formik.handleChange} type="password" name="matKhau" required />
           <label>Password</label>
         </div>
-        <a
-          style={{ width: "70%", margin: "40px auto 0" }}
-          className={styles.form_submit}
-          href="/home"
-        >
+        <button style={{ width: "70%", margin: "40px auto 0" }} className={styles.form_submit}>
           <span />
           <span />
           <span />
           <span />
           Submit
-        </a>
+        </button>
       </form>
     </div>
   );
